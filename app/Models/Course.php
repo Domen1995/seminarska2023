961dfc9895c3cf42bfae53b3681783df0efa32d5
Domen1995 <tapewormerbinkosti@gmail.com>
2023-02-12 19:08:48 +0100

    courses search in DB

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     use HasFactory;
+
+    public static function findMatching($limitations)
+    {
+        return Course::where('name', 'like', "%".$limitations."%")
+                        ->orWhereIn('user_id', User::where('name', 'like', '%'.$limitations.'%')->where('isTeacher', '1'));
+    }
 }
